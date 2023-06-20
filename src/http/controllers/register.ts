d99c19b0f767122a120d944d5cb5a6d2090ed625
Demis Russo <demisjunior@hotmail.com>
2023-06-19 23:04:26 -0300

    add create users route

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+	const schema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6)
+	})
+
+	const { name, email, password } = schema.parse(req.body)
+
+	const userWithSameEmail = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	})
+
+	if (userWithSameEmail) {
+		return reply.status(409).send()
+	}
+
+	const password_hash = await hash(password, 6)
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			password_hash
+		}
+	})
+
+	return reply.status(201).send()
+}
