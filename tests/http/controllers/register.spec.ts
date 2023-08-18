470ba385994815fc085b7610da5633e4d651db7c
Demis Russo <demisjunior@hotmail.com>
2023-08-18 16:53:55 -0300

    add tests for register controller

--- tests/http/controllers/register.spec.ts
+++ tests/http/controllers/register.spec.ts
@@ -1,3 +1,24 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, test, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('OK', () => {})
+describe('Register Controller (e2e)', async () => {
+	beforeAll(async () => {
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
+
+	test('should be able to register', async () => {
+		await request(app.server)
+			.post('/users')
+			.send({
+				name: 'John Doe',
+				email: 'johndoe@test.com',
+				password: 'testing123'
+			})
+			.expect(201)
+	})
+})
