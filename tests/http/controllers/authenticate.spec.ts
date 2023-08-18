f6e7fe182f6f990c17e9fc8fb80390c5e379bc7c
Demis Russo <demisjunior@hotmail.com>
2023-08-18 17:36:30 -0300

    add authenticate e2e tests

--- tests/http/controllers/authenticate.spec.ts
+++ tests/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { test, describe, beforeAll, afterAll, expect } from 'vitest'
+import request from 'supertest'
 
-test('OK', () => {})
+describe('Authenticate Controller (e2e)', () => {
+	beforeAll(async () => {
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
+
+	test('should be able to authenticate', async () => {
+		await request(app.server).post('/users').send({
+			name: 'John Doe',
+			email: 'johndoe@test.com',
+			password: 'testing123'
+		})
+
+		const response = await request(app.server).post('/sessions').send({
+			email: 'johndoe@test.com',
+			password: 'testing123'
+		})
+
+		expect(response.statusCode).toEqual(200)
+		expect(response.body).toEqual({
+			token: expect.any(String)
+		})
+	})
+})
